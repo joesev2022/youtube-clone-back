2506534ead7e13b4abd7d937df393ecbf35e378a
Jose_Apaza <jose.apazasev@gmail.com>
2023-08-12 23:39:54 -0500

    [develop] Added signin functionality on auth controller and installed jwt and cookieparser

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     try {
@@ -10,6 +11,27 @@ export const signup = async (req, res, next) => {
         
         await newUser.save();
         res.status(200).send("User has been created");
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+export const signin = async (req, res, next) => {
+    try {
+        //Buscar al usuario por su username
+        const user = await User.findOne({username: req.body.username});
+        if (!user) return res.status(404).send("User not found");
+        //Comparar la contraseña enviada por el usuario con la contraseña del usuario que se encuentra en la BD.
+        const isCorrect = await bcrypt.compare(req.body.password, user.password);
+        if (!isCorrect) return res.status(400).send("Wrong credentials");
+
+        const token = jwt.sign({id: user._id}, process.env.JWT)
+        const {password, ...others} = user._doc;
+
+        res.cookie("access_token", token, {
+            httpOnly: true,
+        }).status(200).json(others);
     }
     catch (error) {
         next(error);
