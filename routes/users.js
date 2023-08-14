82c262109d3cc89a41328fd8accda5584aefbe23
Jose_Apaza <jose.apazasev@gmail.com>
2023-08-14 12:37:47 -0500

    Adding endpoints on users routes and functions on user controller

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,27 @@
 import express from 'express';
-import { test } from '../controllers/user.js';
+import { deleteUser, getUser, likeVideo, subscribe, unsubscribe, updateUser } from '../controllers/user.js';
 
 const router = express.Router();
 
-router.get('/test', test)
+//update user
+router.put("/:id", updateUser)
+
+//delete user
+router.delete("/:id", deleteUser)
+
+//get a user
+router.get("/find/:id", getUser)
+
+//subscribe a user
+router.put("/sub/:id", subscribe)
+
+//unsubscribe a user
+router.put("/unsub/:id", unsubscribe)
+
+// like a video
+router.put("/like/:videoId", likeVideo)
+
+//dislike a video
+router.put("/dislike/:videoId", likeVideo)
 
 export default router;
