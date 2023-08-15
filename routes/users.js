20371d514f5f7f1a33b908811609ffafe44fa875
Jose_Apaza <jose.apazasev@gmail.com>
2023-08-14 21:05:50 -0500

    [develop] Added verifyTokenjs and callback jwt function on users routes

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { deleteUser, getUser, likeVideo, subscribe, unsubscribe, updateUser } from '../controllers/user.js';
+import { verifyToken } from '../verifyToken.js';
 
 const router = express.Router();
 
 //update user
-router.put("/:id", updateUser)
+router.put("/:id", verifyToken, updateUser)
 
 //delete user
 router.delete("/:id", deleteUser)
